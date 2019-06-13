4120c4cba97cd592d5167bb39b2d82cf490fd12c
Josef Petermann <josef.petermann@eoda.de>
2019-06-13 10:48:29 +0200

    commented out OpenAPI spec part

--- Plumber_APIs_in_R/model-api/entrypoint.R
+++ Plumber_APIs_in_R/model-api/entrypoint.R
@@ -2,30 +2,32 @@ library(plumber)
 
 pr <- plumb("plumber.R")
 
-pr$run(port = 5762,
-      swagger = function(pr, spec, ...){
-        # Define request body for POST to /predict
-        spec$paths$`/predict`$post$requestBody <- list(
-          description = "New data to predict",
-          required = TRUE,
-          content = list(
-            `application/json` = list(
-              # Define JSON schema
-              schema = list(
-                title = "Car",
-                required = c("cyl", "hp"),
-                properties = list(
-                  cyl = list(
-                    type = "integer"
-                  ),
-                  hp = list(
-                    type = "integer"
-                  )
-                )
-              )
-            )
-          )
-        )
-        spec
-      })
+pr$run(port = 5762 )
 
+# commented out to counter "Error in if (swagger) { : argument is not interpretable as logical"
+#
+# swagger = function(pr, spec, ...){
+#   # Define request body for POST to /predict
+#   spec$paths$`/predict`$post$requestBody <- list(
+#     description = "New data to predict",
+#     required = TRUE,
+#     content = list(
+#       `application/json` = list(
+#         # Define JSON schema
+#         schema = list(
+#           title = "Car",
+#           required = c("cyl", "hp"),
+#           properties = list(
+#             cyl = list(
+#               type = "integer"
+#             ),
+#             hp = list(
+#               type = "integer"
+#             )
+#           )
+#         )
+#       )
+#     )
+#   )
+#   spec
+# })
